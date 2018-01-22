aac723d2992c0de34ad58660d243556dff4f810c
Alex <voloshin.sasha777@gmail.com>
2018-01-22 13:32:47 +0100

    Removed comments 4/8

--- app/src/main/java/com/example/admin/testandroidapp/OpenGLRenderer.java
+++ app/src/main/java/com/example/admin/testandroidapp/OpenGLRenderer.java
@@ -25,13 +25,10 @@ public class OpenGLRenderer implements Renderer {
         glViewport(0, 0, width, height);
     }
 
-        public static int loadShader(int type, String shaderCode){
+    public static int loadShader(int type, String shaderCode){
 
-        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
         int shader = GLES20.glCreateShader(type);
 
-        // add the source code to the shader and compile it
         GLES20.glShaderSource(shader, shaderCode);
         GLES20.glCompileShader(shader);
 
